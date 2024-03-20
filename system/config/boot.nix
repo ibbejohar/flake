cac0050f059196d8e0003d2f4ea8486a472543d8
Ibrahim Johar <ibbe.johar@gmail.com>
2024-03-20 13:55:10 +0100

    testing

--- system/config/boot.nix
+++ system/config/boot.nix
@@ -4,12 +4,12 @@
   boot = {
     swraid.enable = false;
     loader = {
-      #systemd-boot.enable = true;
-      #efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
 
-      grub.enable = true;
-      grub.device = "/dev/vda";
-      grub.useOSProber = true;
+      #grub.enable = true;
+      #grub.device = "/dev/vda";
+      #grub.useOSProber = true;
 
     };
     extraModprobeConfig = "options kvm_intel nested=1";
