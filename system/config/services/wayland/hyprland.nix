87dadb6ce545ffecc56858a33f61f0342fd02928
Ibrahim Johar <ibbe.johar@gmail.com>
2024-03-21 11:52:42 +0100

    hyprland nvidia

--- system/config/services/wayland/hyprland.nix
+++ system/config/services/wayland/hyprland.nix
@@ -2,8 +2,17 @@
 
 {
 
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+   enable = true;
+   nvidiaPacthes = true;
+   xwayland.enable = true;
+
+  };
+  
   # Optional, hint electron apps to use wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+
+  hardware..nvidia.modesetting.enable = true;
 
 }
