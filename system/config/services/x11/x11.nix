f317800f1a8c7fe5f6d6958a76a1a675c67f86ff
Ibrahim Johar <ibbe.johar@gmail.com>
2024-10-23 16:27:33 +0200

    samething

--- system/config/services/x11/x11.nix
+++ system/config/services/x11/x11.nix
@@ -13,28 +13,6 @@
     displayManager = {
       defaultSession = "none+dwm";
 
-      lightdm = {
-        enable = true;
-        greeters.mini = {
-          enable = true;
-          user = "fool";
-          extraConfig = ''
-          [greeter]
-          show-password-label = true
-          invalid-password-text = Who The Hell Are You?
-          password-alignment = left
-          [greeter-theme]
-          font = "JetBrains Mono Nerd Font";
-          font-size = 15px
-          text-color = "#FFFFFF"
-          border-color = "#0f0f17"
-          window-color = "#0f0f17"
-          password-background-color = "#0f0f17"
-          password-border-color = "#0f0f17"
-          background-image = "/etc/nixos/wallpaper.jpg"
-          '';
-        };
-      };
     };
     xserver = {
       enable = true;
@@ -43,6 +21,30 @@
       xkb.layout = "se";
       xkb.variant = "nodeadkeys";
 
+      displayManager = {
+        lightdm = {
+          enable = true;
+          greeters.mini = {
+            enable = true;
+            user = "fool";
+            extraConfig = ''
+            [greeter]
+            show-password-label = true
+            invalid-password-text = Who The Hell Are You?
+            password-alignment = left
+            [greeter-theme]
+            font = "JetBrains Mono Nerd Font";
+            font-size = 15px
+            text-color = "#FFFFFF"
+            border-color = "#0f0f17"
+            window-color = "#0f0f17"
+            password-background-color = "#0f0f17"
+            password-border-color = "#0f0f17"
+            background-image = "/etc/nixos/wallpaper.jpg"
+            '';
+          };
+        };
+      };
     };
   };
 }
