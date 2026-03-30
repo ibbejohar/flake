c30690c8bf7d024b48b2f2fda2bc4c1f31fe4d99
Ibrahim Johar <ibbe.johar@gmail.com>
2026-03-30 20:34:33 +0200

    format

--- home/fool/config/nvim/nvim.nix
+++ home/fool/config/nvim/nvim.nix
@@ -1,16 +1,15 @@
 { config, pkgs, inputs, system, ... }:
 let
- neovim-nightly = inputs.neovim-nightly-overlay.packages.${system}.neovim;
+  neovim-nightly = inputs.neovim-nightly-overlay.packages.${system}.neovim;
 in
-{
-	programs.neovim = {
-		enable = true;
-		package  = neovim-nightly;
-		defaultEditor = true;
-		vimAlias = true;
-		viAlias = true;
-                extraLuaPackages = ps: [ ps.magick ];
-                extraPackages =  [ pkgs.imagemagick ];
-	};
-
-}
+  {
+    programs.neovim = {
+      enable = true;
+      package  = neovim-nightly;
+      defaultEditor = true;
+      vimAlias = true;
+      viAlias = true;
+      extraLuaPackages = ps: [ ps.magick ];
+      extraPackages =  [ pkgs.imagemagick ];
+    };
+  }
