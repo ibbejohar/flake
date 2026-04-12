2a9f0dd17578a9082d9cdaf90e1152b9fd8740ce
Ibrahim Johar <ibbe.johar@gmail.com>
2026-04-12 19:37:08 +0200

    vi clear screen

--- home/fool/config/shell/inputrc.nix
+++ home/fool/config/shell/inputrc.nix
@@ -9,6 +9,7 @@
       set show-mode-in-prompt on
       set vi-ins-mode-string \1\e[6 q\2
       set vi-cmd-mode-string \1\e[2 q\2
+      "C-L":clear-screen
       '';
       variables = {
       colored-stats = true;	
