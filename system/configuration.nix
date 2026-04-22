9a4482ec427de43605a1b89625e3504467935917
Ibrahim Johar <ibbe.johar@gmail.com>
2026-04-22 10:39:16 +0200

    remove nix-ld

--- system/configuration.nix
+++ system/configuration.nix
@@ -18,29 +18,6 @@
 
     nixpkgs.config.allowUnfree = true;
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = with pkgs; [
-      libxkbcommon
-      xorg.libX11
-      xorg.libXcursor
-      xorg.libXrandr
-      xorg.libXi
-      xorg.libXext
-      xorg.libxcb
-      xcbutilxrm
-      libxcb-wm
-      libxcb-util
-      libxcb-image
-      libxcb-keysyms
-      libxcb-render-util
-      freetype
-      fontconfig
-      dbus
-      qt6.qtcharts
-      qt6.qtbase
-      qt6.qtlocation
-      qt6.qtpositioning
-    ];
     system.stateVersion = "25.05";
 
   }
